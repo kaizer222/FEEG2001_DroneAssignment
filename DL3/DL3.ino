b2088314ba8ead64597d76b2d6a2ba59756ba4eb
James Tang <70836155+yonghuatang@users.noreply.github.com>
2022-03-22 10:58:28 +0800

    Update DL3.ino

--- DL3/DL3.ino
+++ DL3/DL3.ino
@@ -33,7 +33,7 @@ THE SOFTWARE.
 
 // For debug use
 #define DEBUG {\
-    Serial.print("DEBUG: LINE ");\
+    Serial.print("\nDEBUG: LINE ");\
     Serial.println(__LINE__);\
 }
 
@@ -45,16 +45,9 @@ THE SOFTWARE.
     while(true);\
 }
 
-// #define OUTPUT_READABLE_QUATERNION
-// #define OUTPUT_READABLE_EULER
-#define OUTPUT_READABLE_YAWPITCHROLL
-// #define OUTPUT_READABLE_REALACCEL
-// #define OUTPUT_READABLE_WORLDACCEL
-// #define OUTPUT_TEAPOT
-
-MPU6050 mpu;
+// MPU6050 mpu;
+MPU6050 mpu(0x69);
 File logfile;
-RTC_DS1307 RTC;
 
 #define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno
 bool blinkState = false;
@@ -76,15 +69,12 @@ VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-// Packet structure for InvenSense teapot demonstration
-uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
-
 // how many milliseconds between grabbing data and logging it
-#define LOG_INTERVAL 1000 // how many milliseconds between grabbing data and logging it. mills between entries (reduce to take more/faster data)
-#define SYNC_INTERVAL 1000  // mills between calls to flush() - to write data to the card
-uint32_t syncTime = 0;  // time of last sync()
+//#define LOG_INTERVAL 1000 // how many milliseconds between grabbing data and logging it. mills between entries (reduce to take more/faster data)
+//#define SYNC_INTERVAL 1000  // mills between calls to flush() - to write data to the card
+//uint32_t syncTime = 0;  // time of last sync()
+uint32_t logMillis = millis();
 #define ECHO_TO_SERIAL  // Echo data to serial port
-#define WAIT_TO_START   // wait for serial input in setup()
 const int chipSelect = 10;  // for the data logging shield, use digital pin 10
 
 // Interrupt detection routine
@@ -106,18 +96,11 @@ void setup() {
 
     // 115200 baud is chosen because it is required for Teapot Demo output, but it
     // depends on your project.
-    Serial.begin(115200);
-    delay(500);
-    DEBUG;
-    Serial.print("Current date: ");
-    Serial.println(__DATE__);
-    Serial.print("Current time: ");
-    Serial.println(__TIME__);
+    Serial.begin(38400);
+    delay(1000);  // crucial for stability
 
-#ifdef WAIT_TO_START
-    Serial.println("Type any character to start >>>");
+    Serial.println("Send any character to start >>>");
     while (!Serial.available());
-#endif  // WAIT_TO_START
 
     // Initialise device
     Serial.println(F("Initialising I2C devices..."));
@@ -126,13 +109,7 @@ void setup() {
 
     // Verify connection
     Serial.println(F("Testing device connections..."));
-    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-    // Wait for ready
-    Serial.println(F("\nType any character to begin DMP programming and demo >>>"));
-    while (Serial.available() && Serial.read());  // empty buffer
-    while (!Serial.available());                  // wait for data
-    while (Serial.available() && Serial.read());  // empty buffer again
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful.") : F("MPU6050 connection failed."));
 
     // Load and configure the DMP
     Serial.println(F("Initialising DMP..."));
@@ -182,7 +159,7 @@ void setup() {
     // output, even if you don't use it:
     pinMode(10, OUTPUT);
 
-    // See if the card is present and can be initialised:
+    // See if the card is present and can be initialised
     if (!SD.begin(chipSelect)) {
         cerr("Card failed or not present.");
     }
@@ -195,7 +172,7 @@ void setup() {
         filename[7] = i%10 + '0';
         // only open a new file if it doesn't exist
         if (!SD.exists(filename)) {
-            logfile = SD.open(filename, FILE_WRITE); 
+            logfile = SD.open(filename, FILE_WRITE);
             break;  // leave the loop
         }
     }
@@ -206,29 +183,15 @@ void setup() {
 
     Serial.print("Logging to: ");
     Serial.println(filename);
-
-    // Connect to RTC
-    Wire.begin();  
-    if (!RTC.begin()) {
-        logfile.println("RTC failed");
-#ifdef ECHO_TO_SERIAL
-        Serial.println("RTC failed");
-#endif  //ECHO_TO_SERIAL
-    }
-
-    logfile.println("millis,unix_stamp,datetime,x,y,z");  
-#ifdef ECHO_TO_SERIAL
-    Serial.println("millis,unix_stamp,datetime,x,y,z");
-#endif  //ECHO_TO_SERIAL
-
-    // If you want to set the AREF to something other than 5V
-    // analogReference(EXTERNAL);
+    logfile.println("Program runtime (s), Yaw, Pitch, Roll");
 }
 
 // =======================================================================================================
 
 void loop() {
-    if (!dmpReady) return; // if programming failed, don't try to do anything
+    if (!dmpReady) {
+        cerr("DMP not ready.");
+    }
 
     // wait for MPU interrupt or extra packet(s) available
     while (!mpuInterrupt && fifoCount < packetSize) {
@@ -252,7 +215,7 @@ void loop() {
     if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
         mpu.resetFIFO();
         fifoCount = mpu.getFIFOCount();
-        Serial.println(F("FIFO overflow!"));
+        // Serial.println(F("FIFO overflow!"));
     } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
         while (fifoCount < packetSize) {
             fifoCount = mpu.getFIFOCount();
@@ -261,159 +224,43 @@ void loop() {
         mpu.getFIFOBytes(fifoBuffer, packetSize);
         fifoCount -= packetSize;
 
-#ifdef OUTPUT_READABLE_QUATERNION
-        // Display quaternion values in easy matrix form: w x y z
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        Serial.print("quat\t");
-        Serial.print(q.w);
-        Serial.print("\t");
-        Serial.print(q.x);
-        Serial.print("\t");
-        Serial.print(q.y);
-        Serial.print("\t");
-        Serial.println(q.z);
-#endif  // OUTPUT_READABLE_QUATERNION
-
-#ifdef OUTPUT_READABLE_EULER
-        // Display Euler angles in degrees
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        mpu.dmpGetEuler(euler, &q);
-        Serial.print("euler\t");
-        Serial.print(euler[0] * 180/M_PI);
-        Serial.print("\t");
-        Serial.print(euler[1] * 180/M_PI);
-        Serial.print("\t");
-        Serial.println(euler[2] * 180/M_PI);
-#endif  // OUTPUT_READABLE_EULER
-
-#ifdef OUTPUT_READABLE_YAWPITCHROLL
-        // Display Euler angles in degrees
+        // yaw pitch roll
         mpu.dmpGetQuaternion(&q, fifoBuffer);
         mpu.dmpGetGravity(&gravity, &q);
         mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-#endif  // OUTPUT_READABLE_YAWPITCHROLL
-
-#ifdef OUTPUT_READABLE_REALACCEL
-        // Display real acceleration, adjusted to remove gravity
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        mpu.dmpGetAccel(&aa, fifoBuffer);
-        mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-        Serial.print("areal\t");
-        Serial.print(aaReal.x);
-        Serial.print("\t");
-        Serial.print(aaReal.y);
-        Serial.print("\t");
-        Serial.println(aaReal.z);
-#endif  // OUTPUT_READABLE_REALACCEL
-
-#ifdef OUTPUT_READABLE_WORLDACCEL
-        // Display initial world-frame acceleration, adjusted to remove gravity
-        // and rotated based on known orientation from quaternion
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        mpu.dmpGetAccel(&aa, fifoBuffer);
-        mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-        mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-        Serial.print("aworld\t");
-        Serial.print(aaWorld.x);
-        Serial.print("\t");
-        Serial.print(aaWorld.y);
-        Serial.print("\t");
-        Serial.println(aaWorld.z);
-#endif  // OUTPUT_READABLE_WORLDACCEL
-
-#ifdef OUTPUT_TEAPOT
-        // Display quaternion values in InvenSense Teapot demo format:
-        teapotPacket[2] = fifoBuffer[0];
-        teapotPacket[3] = fifoBuffer[1];
-        teapotPacket[4] = fifoBuffer[4];
-        teapotPacket[5] = fifoBuffer[5];
-        teapotPacket[6] = fifoBuffer[8];
-        teapotPacket[7] = fifoBuffer[9];
-        teapotPacket[8] = fifoBuffer[12];
-        teapotPacket[9] = fifoBuffer[13];
-        Serial.write(teapotPacket, 14);
-        teapotPacket[11]++;
-#endif  // OUTPUT_TEAPOT
+        if (millis()  - logMillis >= 1000) {  // only write to file every one second
+            logfile.print(millis() / 1000);  // seconds since start
+            logfile.print("s,");
+            logfile.print(ypr[0] * 180/M_PI);
+            logfile.print(",");
+            logfile.print(ypr[1] * 180/M_PI);
+            logfile.print(",");
+            logfile.print(ypr[2] * 180/M_PI);
+            logfile.println(",,");
+            logfile.flush();
+            logMillis = millis();
+        }
+#ifdef ECHO_TO_SERIAL
+        Serial.print(millis() / 1000);
+        Serial.print("s\t");          
+        Serial.print("yaw ");
+        Serial.print(ypr[0] * 180/M_PI);
+        Serial.print("\tpitch ");
+        Serial.print(ypr[1] * 180/M_PI);
+        Serial.print("\troll ");
+        Serial.println(ypr[2] * 180/M_PI);
+#endif  // ECHO_TO_SERIAL
 
         // Blink built-in LED to indicate activity
         blinkState = !blinkState;
         digitalWrite(LED_BUILTIN, blinkState);
     }
 
-    DateTime now;
-
     // Delay for the amount of time we want between readings
-    delay((LOG_INTERVAL ) - (millis() % LOG_INTERVAL));
+//    delay((LOG_INTERVAL) - (millis() % LOG_INTERVAL));
 
-    // Log milliseconds since starting
-    uint32_t m = millis();
-    logfile.print(m);       // milliseconds since start
-    logfile.print(", ");    
-#ifdef ECHO_TO_SERIAL
-    Serial.print(m);        // milliseconds since start
-    Serial.print(", ");  
-#endif  // ECHO_TO_SERIAL
-
-    // Fetch the time
-    now = RTC.now();
-    // log time
-    logfile.print(now.unixtime());  // seconds since 1/1/1970
-    logfile.print(", ");
-    logfile.print('"');
-    logfile.print(now.year(), DEC);
-    logfile.print("/");
-    logfile.print(now.month(), DEC);
-    logfile.print("/");
-    logfile.print(now.day(), DEC);
-    logfile.print(" ");
-    logfile.print(now.hour(), DEC);
-    logfile.print(":");
-    logfile.print(now.minute(), DEC);
-    logfile.print(":");
-    logfile.print(now.second(), DEC);
-    logfile.print('"');
-#ifdef ECHO_TO_SERIAL
-    Serial.print(now.unixtime());  // seconds since 1/1/1970
-    Serial.print(", ");
-    Serial.print('"');
-    Serial.print(now.year(), DEC);
-    Serial.print("/");
-    Serial.print(now.month(), DEC);
-    Serial.print("/");
-    Serial.print(now.day(), DEC);
-    Serial.print(" ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(":");
-    Serial.print(now.minute(), DEC);
-    Serial.print(":");
-    Serial.print(now.second(), DEC);
-    Serial.print('"');
-#endif  // ECHO_TO_SERIAL
-
-    logfile.print(", ");    
-    logfile.print("ypr\t");
-    logfile.print(ypr[0] * 180/M_PI);
-    logfile.print("\t");
-    logfile.print(ypr[1] * 180/M_PI);
-    logfile.print("\t");
-    logfile.println(ypr[2] * 180/M_PI);
-#ifdef ECHO_TO_SERIAL
-    Serial.print("ypr\t");
-    Serial.print(ypr[0] * 180/M_PI);
-    Serial.print("\t");
-    Serial.print(ypr[1] * 180/M_PI);
-    Serial.print("\t");
-    Serial.println(ypr[2] * 180/M_PI);
-#endif // ECHO_TO_SERIAL
-
-    logfile.println();
-#ifdef ECHO_TO_SERIAL
-    Serial.println();
-#endif // ECHO_TO_SERIAL
-
-    if ((millis() - syncTime) < SYNC_INTERVAL) return;
-    syncTime = millis();
-    logfile.flush();
+//    if ((millis() - syncTime) < SYNC_INTERVAL) return;
+//    syncTime = millis();
+//    logfile.flush();
+    delay(10);
 }
